19219490d62208d6bc2a1b76cf61dda41f7242f8
Madrobot9182 <65980347+Madrobot9182@users.noreply.github.com>
2024-07-25 15:00:35 -0600

    show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,10 +10,10 @@ interface GameCardProp {
 
 const GameCard = ({ game }: GameCardProp) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width='300px' borderRadius={10} overflow='hidden'>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize='2xl'>{game.name}</Heading>
         <HStack justifyContent='space-between'>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
